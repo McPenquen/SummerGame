2ea7fc39aabfbc836e02f4dcc727117742778d62
McPenquen <majda.calk@outlook.com>
2021-06-12 22:33:19 +0200

    Add jumping only on the environment layer feature

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] public int playerId = 0;
     [SerializeField] public float speed = 5.0f;
     [SerializeField] public int jumpPower = 7;
+    private int canJump = 0;
 
     private void Awake() {
         // Increase the player id based on the player manager
@@ -14,26 +14,26 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Update()
     {
-        // Set the movement based on the playerID
-        if (playerId == 1)
+        // Get the input keys from the horizontal axis based on playerId
+        var movement =  Input.GetAxis("Horizontal" + playerId.ToString());
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
+        // Detect jumping
+        if (Input.GetButtonDown("Jump" + playerId.ToString()) && canJump == 1) 
         {
-            var movement =  Input.GetAxis("Horizontal1");
-            transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
-            // Handle jumping
-            if (Input.GetKeyDown(KeyCode.UpArrow)) 
-            {
-                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1 * jumpPower), ForceMode2D.Impulse);
-            }
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1 * jumpPower), ForceMode2D.Impulse);
         }
-        else 
+    }
+    // collisiond handling inspired from: https://answers.unity.com/questions/1220752/how-to-detect-if-not-colliding.html
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.layer == 11) // 11 is the environment layer
         {
-            var movement = Input.GetAxis("Horizontal2");
-            transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
-            // Handle jumping
-            if (Input.GetKeyDown(KeyCode.W)) 
-            {
-                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1 * jumpPower), ForceMode2D.Impulse);
-            }
+            canJump++;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.layer == 11) // 11 is the environment layer
+        {
+            canJump--;
         }
     }
 }
