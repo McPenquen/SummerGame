780d91160dbb1770dc04c0fcb799914c499ceb4b
McPenquen <majda.calk@outlook.com>
2021-06-16 19:38:02 +0200

    Add simple unrestricted bond

--- Assets/Scripts/PlayersBond.cs
+++ Assets/Scripts/PlayersBond.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayersBond : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+    [SerializeField] private float zLayer = 1.0f;
+    private Vector3 newPosition = new Vector3(0, 0, 0);
+    private Quaternion newRotation = new Quaternion(0, 0, 0, 0);
+    private Vector3 playersVector;
+    private Vector3 bondScale = new Vector3(3, 0.5f, 1);
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        // Update the length of the bond
+        playersVector = player1.transform.position - player2.transform.position;
+        bondScale.x = playersVector.magnitude;
+        transform.localScale = bondScale;
+
+        // Update the bond's bosition
+        newPosition = player1.transform.position + player2.transform.position;
+        newPosition = newPosition / 2;
+        newPosition.z = zLayer;
+        transform.position = newPosition;
+
+        // Rotate the bond
+        newRotation = Quaternion.LookRotation(player1.transform.position - player2.transform.position);
+        newRotation.y = 0;
+        transform.rotation = newRotation;
     }
 }
