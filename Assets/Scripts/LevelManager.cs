8f2ac71a17f8dfd3c53ffdb80c19e3c783b9e835
McPenquen <majda.calk@outlook.com>
2021-07-03 23:42:45 +0200

    Add victory and lost script

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -9,7 +9,16 @@ public class LevelManager : MonoBehaviour
     [Header("Level1")]
     [SerializeField] private GameObject pauseMenuUI = null;
     [SerializeField] private GameObject gameplayUI = null;
+    // Players object
+    [SerializeField] private GameObject players = null;
+    // Current last check point
+    [SerializeField] private GameObject lastCheckpoint = null;
+    // Victory location
+    [SerializeField] private GameObject finishLine = null;
+    // If pause menu is open
     public static bool isPaused = false;
+    // If the level is lost
+    public static bool isLost = false;
     private void Awake()
     {
         pauseMenuUI.SetActive(false);
@@ -28,6 +37,19 @@ public class LevelManager : MonoBehaviour
             {
                 PauseGame();
             }
+        }
+
+        // If players reach the final destination they won
+        if (isWon())
+        {
+            SceneManager.LoadScene("Victory");
+        }
+        // If players fall they lose
+        arePLayersFallen();
+        // If players lose reload them to the last checkpoint
+        if (isLost)
+        {
+            CheckpointReturn();
         }
     }
 
@@ -57,4 +79,27 @@ public class LevelManager : MonoBehaviour
         Time.timeScale = 1;
     }
     // pause menu inspired from: https://www.youtube.com/watch?v=JivuXdrIHK0&t=354s&ab_channel=Brackeys 
+    // Return players to the latest checkpoint
+    private void CheckpointReturn()
+    {
+        players.transform.position = lastCheckpoint.transform.position;
+        isLost = false;
+    }
+    // Returns true if the players reach the final destination
+    private bool isWon()
+    {
+        if (players.transform.position == finishLine.transform.position)
+        {
+            return true;
+        }
+        return false;
+    }
+    // Sets the isLost to true
+    private void arePLayersFallen()
+    {
+        if (players.transform.position.y < -20.0f)
+        {
+            isLost = true;
+        }
+    }
 }
