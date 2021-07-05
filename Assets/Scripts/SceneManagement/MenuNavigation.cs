b734b82f8fa1e028205faae6494d6bf7f69f8e2b
McPenquen <majda.calk@outlook.com>
2021-07-05 23:23:43 +0200

    Add menu keys navigation

---
+++ Assets/Scripts/SceneManagement/MenuNavigation.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuNavigation : MonoBehaviour
+{
+    // Counter of update method
+    private int updateCounter = 0;
+    // Selected Child index
+    private int selectedChildIndex = 0;
+    void Update()
+    {
+        if (updateCounter <= 1)
+        {
+            // The first time around it clears the object from event system
+            if (updateCounter == 0)
+            {
+                EventSystem.current.SetSelectedGameObject(null);
+            }
+            // The second time around it selects a new default object
+            else if (updateCounter == 1)
+            {
+                // Automatically select the first child of the menu
+                EventSystem.current.SetSelectedGameObject(gameObject.transform.GetChild(selectedChildIndex).gameObject);
+            }
+            updateCounter++;
+        }
+        // Navigate up and down in the menu with keys
+        if (Input.GetKeyDown(KeyCode.UpArrow) && selectedChildIndex != 0)
+        {
+            selectedChildIndex--;
+            updateCounter = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && selectedChildIndex != gameObject.transform.childCount - 1)
+        {
+            selectedChildIndex++;
+            updateCounter = 0;
+        }
+    }
+    private void OnDisable() 
+    {
+        // On disable we resetthe update counter
+        updateCounter = 0;
+        selectedChildIndex = 0;
+    }
+}
