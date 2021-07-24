a00e6adf0602f40fcb8706a6337a897e8ad95836
McPenquen <majda.calk@outlook.com>
2021-07-24 16:52:26 +0200

    Few suggested changes for the player movement and bond behaviour

--- Assets/Scripts/Players/Character.cs
+++ Assets/Scripts/Players/Character.cs
@@ -146,21 +146,43 @@ public class Character : MonoBehaviour
 
         // Check if the player is grounded
         m_isGrounded = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - halfHeight - 0.1f), Vector2.down, 0.05f);
-    }
 
-    private void FixedUpdate()
-    {
         // Check if the X input has a value above or less than 0
         if (m_xInput < 0 || m_xInput > 0)
         {
             // Flip sprite depending on value of X input
             GetComponent<SpriteRenderer>().flipX = -m_xInput < 0.0f;
 
-            // Add force to the rigid body based on the player input
-            m_rb.AddForce(new Vector2((m_xInput * m_movementSpeed - m_rb.velocity.x) * m_movementSpeed, 0));
+            if (!m_isSwinging)
+            {
+                Vector3 newPos = transform.position + new Vector3(m_xInput * m_movementSpeed * Time.deltaTime, 0, 0);
+                // Move only when it is allowed
+                if (m_bond.AllowableDistance(newPos, m_otherPlayer.transform.position))
+                {
+                    transform.position = newPos;
+                }
+                // Movement towards the other player is allowed too
+                else if ((newPos - m_otherPlayer.transform.position).magnitude < (transform.position - m_otherPlayer.transform.position).magnitude)
+                {
+                    transform.position = newPos;
+                }
+            }
+            else
+            {
+                // Add force to the rigid body based on the player input
+                m_rb.AddForce(new Vector2((m_xInput * m_movementSpeed - m_rb.velocity.x) * m_movementSpeed, 0));
+                // Alter the rigid body velocity
+                m_rb.velocity = new Vector2(m_rb.velocity.x, m_rb.velocity.y);
+            }
+        }
 
-            // Alter the rigid body veolocuty
-            m_rb.velocity = new Vector2(m_rb.velocity.x, m_rb.velocity.y);
+        // Correct the distance from each other in the air
+        if (((transform.position - m_otherPlayer.transform.position).magnitude >= m_bond.ReturnMaxLength())
+            && !(m_touchingOtherPlayer || m_isGrounded))
+        {
+            m_bond.EnableDistanceJoint();
+            m_bond.UpdateDistance(m_bond.ReturnMaxLength());
+            m_bond.DisableDistanceJoint();
         }
 
         // Check if player is grounded or touching other player
@@ -180,7 +202,7 @@ public class Character : MonoBehaviour
                 // Disable the walking animation
                 m_playerAnimator.SetBool("isWalking", false);
             }
-
+            
             // Check if player is at the right of the other player
             if (GetPlayerPosition().x >= m_otherPlayer.GetPlayerPosition().x)
             {
@@ -214,6 +236,7 @@ public class Character : MonoBehaviour
                     m_bond.ExtendBond(Time.deltaTime * m_movementSpeed);
                 }
             }
+        
         }
 
         // Check if the player has walked off the edge of a platform
