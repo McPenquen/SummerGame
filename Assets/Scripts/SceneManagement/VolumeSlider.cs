428bcb876679bb33ae950b93d836acf89005d63f
McPenquen <majda.calk@outlook.com>
2021-08-04 19:49:43 +0200

    Add scnd slider for sounds effects

--- Assets/Scripts/SceneManagement/VolumeSlider.cs
+++ Assets/Scripts/SceneManagement/VolumeSlider.cs
@@ -2,28 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Types of volumes
+enum VolumeType {
+    SFXVolume,
+    MusicVolume
+}
+
 public class VolumeSlider : SliderManager
 {
-    public float mainVolume = 0;
+    public float volume = 0;
     private float returnVal = 0;
+    // The type of volume to control
+    [SerializeField] private VolumeType chosenVolumeType = VolumeType.MusicVolume;
+
     private void Start()
     {
         base.Start();
-        // Load the slider value to be the one of the Master_Volume RTPC
+        // Load the slider value based on the volume type
         int valueType = (int)AkQueryRTPCValue.RTPCValue_Global; 
-        AkSoundEngine.GetRTPCValue("Master_Volume", null, 0, out returnVal, ref valueType);
+        if (chosenVolumeType== VolumeType.SFXVolume)
+        {
+            AkSoundEngine.GetRTPCValue("SFX_Volume", null, 0, out returnVal, ref valueType);
+        }
+        else
+        {
+            AkSoundEngine.GetRTPCValue("Music_Volume", null, 0, out returnVal, ref valueType);
+        }
         slider.value = returnVal;
     }
     // RTPC-getter inspired from: https://www.audiokinetic.com/qa/5592/need-understanding-getrtpcvalue-updating-rtpcvalue-unity
     private void Update()
     {
         base.Update();
-        mainVolume = slider.value;
+        volume = slider.value;
 
-        // Update all RTCP values to the sliders value
-        AkSoundEngine.SetRTPCValue("Music_Volume", mainVolume);
-        AkSoundEngine.SetRTPCValue("SFX_Volume", mainVolume);
-        AkSoundEngine.SetRTPCValue("Master_Volume", mainVolume);        
+        // Update the chosen RTCP value to the sliders value
+        if (chosenVolumeType== VolumeType.SFXVolume)
+        {
+           AkSoundEngine.SetRTPCValue("SFX_Volume", volume);  
+        }
+        else
+        {
+            AkSoundEngine.SetRTPCValue("Music_Volume", volume); 
+        }      
     }   
 }
 // Qorking with wwise inspired from: https://www.youtube.com/watch?v=YUlVlx4hHkU&ab_channel=DanielKim
